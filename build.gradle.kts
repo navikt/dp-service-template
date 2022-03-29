45c0947b0b53a3f4fd675f089b2a6e4226730294
André Roaldseth <andre.roaldseth@nav.no>
2022-03-29 10:01:53 +0200

    Oppdater gradle.build.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,73 +1,39 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
     kotlin("jvm") version Kotlin.version
-    id(Spotless.spotless) version Spotless.version
-    id(Shadow.shadow) version Shadow.version
-}
-
-buildscript {
-    repositories {
-        mavenCentral()
-    }
-}
-
-apply {
-    plugin(Spotless.spotless)
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
 
 repositories {
     mavenCentral()
-    maven(url = "https://packages.confluent.io/maven/")
     maven("https://jitpack.io")
 }
 
 application {
-    applicationName = "dp-SERVICENAME"
-    mainClassName = "no.nav.dagpenger.SERVICENAME"
+    mainClass.set("no.nav.dagpenger.SERVICENAME")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
-
-tasks.withType<KotlinCompile>().all {
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+kotlin {
+    jvmToolchain {
+        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-
     implementation(RapidAndRivers)
 
     implementation(Konfig.konfig)
     implementation(Kotlin.Logging.kotlinLogging)
 
     testImplementation(kotlin("test"))
-    testImplementation(Junit5.api)
-    testImplementation(KoTest.runner)
-    testRuntimeOnly(Junit5.engine)
-}
-
-spotless {
-    kotlin {
-        ktlint(Ktlint.version)
-    }
-    kotlinGradle {
-        target("*.gradle.kts", "buildSrc/**/*.kt*")
-        ktlint(Ktlint.version)
-    }
-}
-
-tasks.named("compileKotlin") {
-    dependsOn("spotlessCheck")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+
     testLogging {
         showExceptions = true
         showStackTraces = true
