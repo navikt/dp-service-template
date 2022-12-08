e9ab41daafbe492ebd755a358739967975961183
Giao The Cung <giaothe@gmail.com>
2022-12-08 20:55:50 +0100

    Bump ktor
    Co-authored-by: Celine Dam <celine.ten.dam@nav.no>

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -165,7 +165,7 @@ object Ktor {
 }
 
 object Ktor2 {
-    const val version = "2.1.3"
+    const val version = "2.2.1"
     object Client {
         fun library(name: String) = "io.ktor:ktor-client-$name:$version"
     }
