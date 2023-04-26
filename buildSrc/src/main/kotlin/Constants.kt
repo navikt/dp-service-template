08763a4492baa92e48b58fa743c6e331989e117f
Marius Eriksen <marius.eriksen@nav.no>
2023-04-26 11:58:41 +0200

    Oppgrader rapids and rivers ktor 2 til nyeste versjon
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -228,7 +228,7 @@ object Prometheus {
 }
 
 const val RapidAndRivers = "com.github.navikt:rapids-and-rivers:2022.03.25-09.17.0628678a7192"
-const val RapidAndRiversKtor2 = "com.github.navikt:rapids-and-rivers:2023042609401682494832.502ede9d3e7e"
+const val RapidAndRiversKtor2 = "com.github.navikt:rapids-and-rivers:2023042611061682500003.f24c0756e00a"
 
 object Slf4j {
     const val version = "1.7.25"
