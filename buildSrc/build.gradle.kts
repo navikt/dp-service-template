46261ad174a4fffce294cbe31ab6942bf83c9efa
Giao The Cung <giaothe@gmail.com>
2025-03-05 08:34:35 +0100

    Kjøre ktlintCheck før check
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     `kotlin-dsl`
     id("org.jlleitschuh.gradle.ktlint") version "12.2.0"
@@ -11,4 +13,12 @@ repositories {
 dependencies {
     implementation(kotlin("gradle-plugin"))
     implementation("org.jlleitschuh.gradle:ktlint-gradle:12.1.0")
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    dependsOn("ktlintFormat")
+}
+
+tasks.named("check") {
+    dependsOn("ktlintCheck")
 }
